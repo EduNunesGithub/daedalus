6b36e254bd495eb1719819665d49877638c1de3c
Edu Nunes <edu.nunes.github@gmail.com>
2026-03-23 22:37:28 -0300

    feat(app): add react query

---
+++ src/app/get-query-client.ts
@@ -0,0 +1,32 @@
+import {
+  defaultShouldDehydrateQuery,
+  environmentManager,
+  QueryClient,
+} from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      dehydrate: {
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+        shouldRedactErrors: () => false,
+      },
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (environmentManager.isServer()) {
+    return makeQueryClient();
+  }
+
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+}
---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { getQueryClient } from "@/app/get-query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
